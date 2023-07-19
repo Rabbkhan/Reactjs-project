5c09f6c396e6a4769007fe3aa7802019ac028fdf
Rabbil khan <rabbilkhan125@gmail.com>
2023-07-19 21:37:56 +0530

    pushing first components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExpenseItems from './components/ExpenseItems'
+
+const App = () => {
+  return (
+    <>
+    <ExpenseItems/> 
+    </>
+ )
+}
+
+export default App
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ExpenseItems = () => {
+  return (
+    <>
+    <div>Date</div>
+    <div>
+        <h2>Food</h2>
+      <div> Rs 10</div> 
+        </div>
+    <div>
+        <h2>Petrol</h2>
+      <div> Rs 100</div> 
+        </div>
+    <div>
+        <h2>Movies</h2>
+      <div> Rs 200</div> 
+        </div>
+
+
+    </>
+  )
+}
+
+export default ExpenseItems
