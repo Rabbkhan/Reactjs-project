f0d95d8d66b19bfe343977299fff554d48866cb8
Rabbil khan <rabbilkhan125@gmail.com>
2023-07-21 16:06:46 +0530

    adding components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from 'react'
 import ExpenseItems from './components/ExpenseItems'
 
 const App = () => {
+  const expense = [
+    {id:'e1', title:'Food', amount: 294.67, date:new Date(2023, 7, 20)},
+    {id:'e2', title:'Mobile', amount: 894.67, date:new Date(2023, 6, 20)},
+    {id:'e3', title:'Car', amount: 9894.67, date:new Date(2023, 5, 20)},
+  ];
   return (
     <>
-    <ExpenseItems/> 
+    <h1>Lets make a expense Tracker</h1>
+
+    <ExpenseItems 
+    title ={expense[0].title}
+    amount={expense[0].amount}
+     date={expense[0].date}/> 
+
+    <ExpenseItems 
+    title ={expense[1].title}
+    amount={expense[1].amount}
+     date={expense[1].date}/> 
+
+    <ExpenseItems
+     title ={expense[2].title}
+     amount={expense[2].amount}
+     date={expense[2].date}/> 
+   
     </>
  )
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,21 +1,33 @@
 import React from 'react'
-
-const ExpenseItems = () => {
+import './ExpenseItem.css'
+const ExpenseItems =(props)=> {
+  
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+  const year = props.date.getFullYear();
   return (
     <>
-    <div>Date</div>
-    <div>
-        <h2>Food</h2>
-      <div> Rs 10</div> 
+    
+<div className='container'>
+    <div className='date'>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+      </div>
+    {/* <div>LocationOfExpenditure</div> */}
+    <div className='expensesection'>
+        <h3>{props.title}</h3>
+       <div className='price'>${props.amount}</div> 
         </div>
-    <div>
+</div>
+    {/* <div>
         <h2>Petrol</h2>
       <div> Rs 100</div> 
         </div>
     <div>
         <h2>Movies</h2>
-      <div> Rs 200</div> 
-        </div>
+      <div> Rs 200</div>  
+  </div>*/}
 
 
     </>
