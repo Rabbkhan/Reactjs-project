b5dfbb49b8376a7244504c47f25d44a6bc24bfc9
Rabbil khan <rabbilkhan125@gmail.com>
2023-07-26 12:28:05 +0530

    adding event listener

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,33 +1,25 @@
 import React from 'react'
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 const ExpenseItems =(props)=> {
   
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const year = props.date.getFullYear();
+const clickHandler =()=>{
+  console.log('clicked');
+}
   return (
     <>
     
 <div className='container'>
-    <div className='date'>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
-      </div>
+    
+      <ExpenseDate date={props.date}/>
     {/* <div>LocationOfExpenditure</div> */}
     <div className='expensesection'>
         <h3>{props.title}</h3>
        <div className='price'>${props.amount}</div> 
         </div>
+<button onClick={clickHandler}>change title</button>
 </div>
-    {/* <div>
-        <h2>Petrol</h2>
-      <div> Rs 100</div> 
-        </div>
-    <div>
-        <h2>Movies</h2>
-      <div> Rs 200</div>  
-  </div>*/}
+   
 
 
     </>
